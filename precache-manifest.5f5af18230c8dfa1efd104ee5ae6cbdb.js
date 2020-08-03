7315ce41054e4fc0626a2928389bfd886837a5de
Jinson Abraham <web2feel@gmail.com>
2020-08-03 19:45:19 +0530

    Updates

---
+++ precache-manifest.5f5af18230c8dfa1efd104ee5ae6cbdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397da80658e8cd81bcc51444fe07b23f",
+    "url": "/pxtorem/index.html"
+  },
+  {
+    "revision": "2ef1c35d2293a0d3d86f",
+    "url": "/pxtorem/static/css/main.e2d3cb74.chunk.css"
+  },
+  {
+    "revision": "e590bc7bd1e34421fa28",
+    "url": "/pxtorem/static/js/2.56842a84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pxtorem/static/js/2.56842a84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ef1c35d2293a0d3d86f",
+    "url": "/pxtorem/static/js/main.29916618.chunk.js"
+  },
+  {
+    "revision": "105fd1ee23857785a534",
+    "url": "/pxtorem/static/js/runtime-main.bbc96db0.js"
+  },
+  {
+    "revision": "c9701157d4758785e5def135e862e3df",
+    "url": "/pxtorem/static/media/logo.c9701157.svg"
+  }
+]);
